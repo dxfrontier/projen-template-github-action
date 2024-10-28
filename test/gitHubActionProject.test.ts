c4fdd3e91942eceb60222f2eeff8b6169acb7039
Mathias von Kaiz <127110360+mvk-abs@users.noreply.github.com>
2024-10-28 16:21:18 +0000

    test: devcontainer extensions

--- test/gitHubActionProject.test.ts
+++ test/gitHubActionProject.test.ts
@@ -186,5 +186,78 @@ describe('GitHubActionProject', (): void => {
       };
       expect(snapshot['.devcontainer.json'].features).toStrictEqual(expectedFeatures);
     });
+
+    test('Container VS Code extensions are set propertly', (): void => {
+      // GIVEN
+      const project = new GitHubActionProject(props);
+
+      // WHEN
+      snapshot = synthSnapshot(project);
+
+      // THEN
+      const expectedExtensions: string[] = [
+        '// Terraform',
+        'HashiCorp.terraform',
+        '',
+        '// Jest',
+        'Orta.vscode-jest',
+        'firsttris.vscode-jest-runner',
+        '',
+        // Rest
+        'humao.rest-client',
+        '',
+        '// VSCode',
+        'aaron-bond.better-comments',
+        'alefragnani.Bookmarks',
+        'alefragnani.project-manager',
+        '',
+        '// NPM',
+        'christian-kohler.npm-intellisense',
+        'mskelton.npm-outdated',
+        '',
+        '// Theme',
+        'PKief.material-icon-theme',
+        'zhuangtongfa.material-theme',
+        'GitHub.github-vscode-theme',
+        '',
+        '// Docker',
+        'ms-vscode-remote.remote-containers',
+        '',
+        '// .env',
+        'mikestead.dotenv', //DotENV
+        '',
+        '// TypeScript',
+        'usernamehw.errorlens',
+        'dbaeumer.vscode-eslint',
+        'oderwat.indent-rainbow',
+        'esbenp.prettier-vscode',
+        'YoavBls.pretty-ts-errors',
+        'streetsidesoftware.code-spell-checker',
+        'wayou.vscode-todo-highlight',
+        'mike-co.import-sorter',
+        'VisualStudioExptTeam.vscodeintellicode',
+        '',
+        '// XML & YAML',
+        'redhat.vscode-yaml',
+        'DotJoshJohnson.xml',
+        '',
+        '// Git',
+        'waderyan.gitblame',
+        'donjayamanne.githistory',
+        'GitHub.vscode-pull-request-github',
+        '',
+        '// README',
+        'yzhang.markdown-all-in-one',
+        'DavidAnson.vscode-markdownlint',
+        'bierner.jsdoc-markdown-highlighting',
+        '',
+        '// Others',
+        'VisualStudioExptTeam.vscodeintellicode',
+        'christian-kohler.path-intellisense',
+        'AykutSarac.jsoncrack-vscode',
+        'tamasfe.even-better-toml',
+      ];
+      expect(snapshot['.devcontainer.json'].extensions).toStrictEqual(expectedExtensions);
+    });
   });
 });
