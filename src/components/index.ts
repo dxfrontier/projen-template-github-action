168608f038600fbc230d0f6dd513a57028da7447
Mathias von Kaiz <127110360+mvk-abs@users.noreply.github.com>
2024-11-06 08:40:40 +0000

    feature: husky

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from './DevContainerComponent';
 export * from './VsCodeComponent';
 export * from './PrettierComponent';
 export * from './GitHubComponent';
+export * from './HuskyComponent';
