7255139468afacb8b4a05418156ce26622ad4f57
Mathias von Kaiz <127110360+mvk-abs@users.noreply.github.com>
2024-11-14 12:47:44 +0000

    chore(build): execute build

--- lib/base/vscode.d.ts
+++ lib/base/vscode.d.ts
@@ -1,23 +1,23 @@
 import { Builder } from './builder';
 import { TypeScriptProjectBase } from './project';
 /**
- * Base class for VSCode builder implementing all relevant configuration.
+ * Base class for VsCode builder implementing all relevant configuration.
  * @abstract
  */
-export declare abstract class VSCodeBase extends Builder {
+export declare abstract class VsCodeBase extends Builder {
     /**
-     * Initializes the base VSCode builder.
-     * @param project The project to configure VSCode builder for.
+     * Initializes the base VsCode builder.
+     * @param project The project to configure VsCode builder for.
      */
     constructor(project: TypeScriptProjectBase);
     /**
-     * File path to the VSCode settings configuration.
+     * File path to the VsCode settings configuration.
      * @return File path to settings file.
      * @protected
      */
     protected get settingsFilePath(): string;
     /**
-     * Settings to be installed in VSCode.
+     * Settings to be installed in VsCode.
      * @return Entries for the settings file.
      * @protected
      */
