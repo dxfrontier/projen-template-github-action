f66963149e10647019294ae35231669fea7d7f93
Mathias von Kaiz <127110360+mvk-abs@users.noreply.github.com>
2024-12-17 16:24:47 +0000

    test(build): projen

---
+++ lib/util/utils.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Converts the first letter of a given string to lowercase while keeping the rest unchanged.
+ * @param phrase The input string whose first letter should be converted to lowercase.
+ * @returns The modified string with the first letter in lowercase.
+ */
+export declare function lowerCaseFirstLetter(phrase: string): string;
